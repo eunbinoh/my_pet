6fe5b45c79115a5b07fe82e4b07a3c5f101754ed
eunbinoh <nohonbi01@gmail.com>
2025-04-11 08:53:46 +0900

    side-inline max-width 지정, 글자수초과 스타일 hidden

--- src/entities/items.tsx
+++ src/entities/items.tsx
@@ -1,35 +1,35 @@
 export const items = {
   recommendItems : [
     {
-      id: 'item0000001',
+      id: 'item000001',
       brandName: 'BRAND_NAME1',
       itemName: '차량용 강아지 방석',
       price: 129000,
       imgUrl: ''
     },
     {
-      id: 'item0000002',
+      id: 'item000002',
       brandName: 'BRAND_NAME2',
       itemName: '고급 케이지 ',
       price: 219000,
       imgUrl: ''
     },
     {
-      id: 'item0000003',
+      id: 'item000003',
       brandName: 'BRAND_NAME3',
       itemName: '대형견 에티켓 넥카라',
       price: 12000,
       imgUrl: ''
     },
     {
-      id: 'item0000004',
+      id: 'item000004',
       brandName: 'BRAND_NAME4',
       itemName: '강아지/고양이 공용 수제 간식',
       price: 49000,
       imgUrl: ''
     },
     {
-      id: 'item0000005',
+      id: 'item000005',
       brandName: 'BRAND_NAME5',
       itemName: '산책 줄 마우스 마스크',
       price: 129000,
@@ -38,7 +38,7 @@ export const items = {
   ],
   recommendHospitals : [
     {
-      id: 'item0000001',
+      id: 'hospital01',
       place: '위치1',
       name: '월드펫동물종합병원',
       score: 4.7,
@@ -46,7 +46,7 @@ export const items = {
       imgUrl: ''
     },
     {
-      id: 'item0000002',
+      id: 'hospital02',
       place: '위치2',
       name: '펫러브동물병원',
       score: 4.7,
@@ -54,7 +54,7 @@ export const items = {
       imgUrl: ''
     },
     {
-      id: 'item0000003',
+      id: 'hospital03',
       place: '위치3',
       name: '24시응급펫센터',
       score: 4.7,
@@ -62,7 +62,7 @@ export const items = {
       imgUrl: ''
     },
     {
-      id: 'item0000004',
+      id: 'hospital04',
       place: '위치4',
       name: '강아지동물병원',
       score: 4.7,
@@ -70,7 +70,7 @@ export const items = {
       imgUrl: ''
     },
     {
-      id: 'item0000005',
+      id: 'hospital05',
       place: '위치5',
       name: '고양이종합병원',
       score: 4.7,
@@ -80,7 +80,7 @@ export const items = {
   ],
   hotItems : [
     {
-      id: 'item0000001',
+      id: 'hot0000001',
       brandName: 'BRAND_NAME1',
       itemName: '차량용 강아지 방석',
       price: 129000,
@@ -89,7 +89,7 @@ export const items = {
       reviews: 99,
     },
     {
-      id: 'item0000002',
+      id: 'hot0000002',
       brandName: 'BRAND_NAME2',
       itemName: '고급 케이지 ',
       price: 219000,
@@ -98,7 +98,7 @@ export const items = {
       reviews: 99,
     },
     {
-      id: 'item0000003',
+      id: 'hot0000003',
       brandName: 'BRAND_NAME3',
       itemName: '대형견 에티켓 넥카라',
       price: 12000,
@@ -107,7 +107,7 @@ export const items = {
       reviews: 99,
     },
     {
-      id: 'item0000004',
+      id: 'hot0000004',
       brandName: 'BRAND_NAME4',
       itemName: '강아지/고양이 공용 수제 간식',
       price: 49000,
@@ -116,9 +116,27 @@ export const items = {
       reviews: 99,
     },
     {
-      id: 'item0000005',
+      id: 'hot0000005',
       brandName: 'BRAND_NAME5',
       itemName: '산책 줄 마우스 마스크',
+      price: 129000,
+      imgUrl: '',
+      score: 4.7,
+      reviews: 99,
+    },
+    {
+      id: 'hot0000006',
+      brandName: 'BRAND_NAME6',
+      itemName: '휴대용 배변패드',
+      price: 129000,
+      imgUrl: '',
+      score: 4.7,
+      reviews: 99,
+    },
+    {
+      id: 'hot0000007',
+      brandName: 'BRAND_NAME7',
+      itemName: '산책용 물그릇',
       price: 129000,
       imgUrl: '',
       score: 4.7,
@@ -127,7 +145,7 @@ export const items = {
   ],
   newItems : [
     {
-      id: 'item0000001',
+      id: 'new0000001',
       brandName: 'BRAND_NAME1',
       itemName: '차량용 강아지 방석',
       price: 129000,
@@ -136,7 +154,7 @@ export const items = {
       reviews: 99,
     },
     {
-      id: 'item0000002',
+      id: 'new0000002',
       brandName: 'BRAND_NAME2',
       itemName: '고급 케이지 ',
       price: 219000,
@@ -145,7 +163,7 @@ export const items = {
       reviews: 99,
     },
     {
-      id: 'item0000003',
+      id: 'new0000003',
       brandName: 'BRAND_NAME3',
       itemName: '대형견 에티켓 넥카라',
       price: 12000,
@@ -154,7 +172,7 @@ export const items = {
       reviews: 99,
     },
     {
-      id: 'item0000004',
+      id: 'new0000004',
       brandName: 'BRAND_NAME4',
       itemName: '강아지/고양이 공용 수제 간식',
       price: 49000,
@@ -163,9 +181,27 @@ export const items = {
       reviews: 99,
     },
     {
-      id: 'item0000005',
+      id: 'new0000005',
       brandName: 'BRAND_NAME5',
       itemName: '산책 줄 마우스 마스크',
+      price: 129000,
+      imgUrl: '',
+      score: 4.7,
+      reviews: 99,
+    },
+    {
+      id: 'new0000006',
+      brandName: 'BRAND_NAME6',
+      itemName: '휴대용 배변패드',
+      price: 129000,
+      imgUrl: '',
+      score: 4.7,
+      reviews: 99,
+    },
+    {
+      id: 'new0000007',
+      brandName: 'BRAND_NAME7',
+      itemName: '산책용 물그릇',
       price: 129000,
       imgUrl: '',
       score: 4.7,
